5a8ae45f682eacbf636b5a78cab6ffa652658bd1
HARSH SINGH <41187121+HARSHSINGH0@users.noreply.github.com>
2020-02-28 23:06:31 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ dict={0:'Harsh',1:'brendon',2:'alfaiz',3:'siddhant',4:'pawan'}
 dictmarks={0:10,1:50,2:60,3:70,4:35}
 dictsubject={0:'biology',1:'economics',2:'computer science',3:'history',4:'physics'}
 dictcity={0:'mumbai',1:'delhi',2:'trivandrum',3:'bengaluru',4:'kolkata'}
-for i in range(20):
+for i in range(200):#number of time you want to insert
     a=random.randrange(0,4)
     b=random.randrange(0,4)
     c=random.randrange(0,4)
