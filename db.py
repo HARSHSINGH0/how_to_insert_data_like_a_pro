aedabc2f4214d30265b6c536951e1633c82ef2d3
HARSH SINGH <41187121+HARSHSINGH0@users.noreply.github.com>
2020-02-28 22:42:04 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,16 @@
+import random
+dict={0:'Harsh',1:'brendon',2:'alfaiz',3:'siddhant',4:'pawan'}
+dictmarks={0:10,1:50,2:60,3:70,4:35}
+dictsubject={0:'biology',1:'economics',2:'computer science',3:'history',4:'physics'}
+dictcity={0:'mumbai',1:'delhi',2:'trivandrum',3:'bengaluru',4:'kolkata'}
+for i in range(20):
+    a=random.randrange(0,4)
+    b=random.randrange(0,4)
+    c=random.randrange(0,4)
+    d=random.randrange(0,4)
+    name=dict[a]
+    marks=dictmarks[b]
+    subject=dictsubject[c]
+    city=dictcity[d]
+    print("insert into emp values(",i,",'{}',{},'{}','{}')"
+        .format(name,marks,subject,city))
